9f8d02f5af32047682a0dacf6b04149813832b5e
greg <gregory.cusack@colorado.edu>
2022-08-22 10:13:52 -0500

    add in time range for query

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 import json
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import connected_components
+import sys
+from datetime import datetime
 
 class Graph_struct:
    def __init__(self, V):
@@ -113,23 +115,35 @@ def parse_result(result):
 
 
 
-def query(client):
+def query(client, t0, t1):
+    # result = client.query('SELECT time, host, peers FROM "gossip-peers" \
+    #     where time > \'2022-08-22T14:16:51.00Z\' and time < \'2022-08-22T14:25:38.00Z\' order by time asc')
     result = client.query('SELECT time, host, peers FROM "gossip-peers" \
-        where time > \'2022-08-19T16:20:30Z\' and time < \'2022-08-19T16:30:32Z\' order by time asc')
+        where time > \'' + t0 + '\' and time < \'' + t1 + '\' order by time asc')    
     # result = client.query('SELECT time, host, peers FROM "gossip-peers" \
     #     where host=\'Am5n3J8Qym76r1uaCyZV3WMrmgg8zbPDKWDco72jV5zq\' and \
     #     time > \'2022-08-19T16:20:30Z\' and time < \'2022-08-19T16:20:32Z\' order by time asc')
     # result = client.query('SELECT count(host), count(peers) FROM "gossip-peers" where host=\'Am5n3J8Qym76r1uaCyZV3WMrmgg8zbPDKWDco72jV5zq\' group by host, time(1s)')
+    # result = client.query('select time, host, peers from "gossip-peers" order by time desc limit 10')
     parse_result(result) 
 
 
 
 def run():
+    if len(sys.argv) < 3:
+        print("ERROR: need to enter start and end timestamp to query for connected components graph")
+        sys.exit(-1)
+    
+    t0 = sys.argv[1]
+    t1 = sys.argv[2]
+    t0_dt = datetime.strptime(t0, '%Y-%m-%dT%H:%M:%SZ')
+    t1_dt = datetime.strptime(t1, '%Y-%m-%dT%H:%M:%SZ')
+
     client = InfluxDBClient(host='localhost', port=8087)
     databases = client.get_list_database()
     client.switch_database('gossipDb')
 
-    query(client)    
+    query(client, t0, t1)    
 
 
 if __name__ == '__main__':
